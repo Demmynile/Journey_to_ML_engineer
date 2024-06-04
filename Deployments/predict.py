f877398976421dd8cdb001217565efc1ac820eb0
Ademola <ademola@ademolas-mbp.home>
2024-06-04 13:32:43 +0100

    add new data information

--- Deployments/predict.py
+++ Deployments/predict.py
@@ -3,7 +3,7 @@ import pickle
 from flask import Flask
 from flask import request
 from flask import jsonify
-
+  
 app = Flask('churn')
 
 model_file = 'model_C=1.0.bin'
@@ -18,10 +18,12 @@ def predict():
     y_pred = model.predict_proba(X)[0,1]
     churn = y_pred >= 0.5
     result = {
-     'churn_probability' : float(y_pred),
+     'churn_probability' : float(y_pred), 
      'churn' : bool(churn)
     }
     return jsonify(result)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True , host='0.0.0.0', port=9696)
